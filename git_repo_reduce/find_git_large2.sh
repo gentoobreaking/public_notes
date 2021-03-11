6f600fa94b4fc731a565f2ce796aa03d57698821
David <david@funpodium.net>
2021-03-11 15:14:36 +0800

    sync notes

---
+++ git_repo_reduce/find_git_large2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+linux_ver()
+{
+git rev-list --objects --all \
+| git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' \
+| awk '/^blob/ {print substr($0,6)}' \
+| sort --numeric-sort --key=2 \
+| cut --complement --characters=13-40 \
+| numfmt --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+}
+
+macos_ver()
+{
+git rev-list --objects --all \
+| git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' \
+| awk '/^blob/ {print substr($0,6)}' \
+| sort --numeric-sort --key=2 \
+| gcut --complement --characters=13-40 \
+| gnumfmt --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+}
+
+#linux_ver
+macos_ver
+
